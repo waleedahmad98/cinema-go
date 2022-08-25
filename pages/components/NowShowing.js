f4c89179353a1ad57daf575d7ebae22e8beddebc
waleedahmad98 <w.ahmad1024@gmail.com>
2022-08-25 15:44:52 +0500

    first

---
+++ pages/components/NowShowing.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function NowShowing() {
+  return (
+    <div>NowShowing</div>
+  )
+}
+
+export async function getStaticProps(){
+    const response = await fetch("")
+}
